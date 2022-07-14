3512de9e39e8cd9ae63aea53aa40700ccd503e9f
yinmin <alice0050722@gmail.com>
2022-07-15 00:56:23 +0800

    Update 2022-07-14T16:56:20.024Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,24 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('.dropdown-menu .dropdown-item').forEach(function (element) {
+    console.log(element);
+    element.addEventListener('click', function (e) {
+      var nextEl = element.nextElementSibling;
+      var parentEl = element.parentElement;
+      var allSubmenus_array = parentEl.querySelectorAll('.submenu');
+
+      if (nextEl && nextEl.classList.contains('submenu')) {
+        e.preventDefault();
+        event.stopPropagation();
+
+        if (nextEl.style.display == 'block') {
+          nextEl.style.display = 'none';
+        } else {
+          nextEl.style.display = 'block';
+        }
+      }
+    });
+  });
 });
 //# sourceMappingURL=all.js.map
