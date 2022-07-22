b0f9c649c958f1c3e2b8e369375e68e357862cb2
yinmin <alice0050722@gmail.com>
2022-07-22 17:03:17 +0800

    Update 2022-07-22T09:03:13.904Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,24 +1,24 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll('.dropdown-menu .dropdown-item').forEach(function (element) {
-    console.log(element);
-    element.addEventListener('click', function (e) {
-      var nextEl = element.nextElementSibling;
-      var parentEl = element.parentElement;
-      var allSubmenus_array = parentEl.querySelectorAll('.submenu');
+  if (window.innerWidth < 992) {
+    document.querySelectorAll('.dropdown-menu .dropdown-item').forEach(function (element) {
+      // console.log(element);
+      element.addEventListener('click', function (e) {
+        var nextEl = element.nextElementSibling;
 
-      if (nextEl && nextEl.classList.contains('submenu')) {
-        e.preventDefault();
-        event.stopPropagation();
+        if (nextEl && nextEl.classList.contains('submenu')) {
+          e.preventDefault();
+          e.stopPropagation(); // 如果點擊時「選單是開啟的」，那就把它關掉
 
-        if (nextEl.style.display == 'block') {
-          nextEl.style.display = 'none';
-        } else {
-          nextEl.style.display = 'block';
+          if (nextEl.style.display == 'block') {
+            nextEl.style.display = 'none';
+          } else {
+            nextEl.style.display = 'block';
+          }
         }
-      }
+      });
     });
-  });
+  }
 });
 //# sourceMappingURL=all.js.map
