17a88c3a09b6311b272eb4207569bc5a3774e089
yinmin <alice0050722@gmail.com>
2022-07-22 17:04:03 +0800

    first stage 1-1

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,21 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.innerWidth < 992) {
+    document.querySelectorAll('.dropdown-menu .dropdown-item').forEach(function (element) {
+      // console.log(element);
+      element.addEventListener('click', function (e) {
+        var nextEl = element.nextElementSibling;
+
+        if (nextEl && nextEl.classList.contains('submenu')) {
+          e.preventDefault();
+          e.stopPropagation();
+          // 如果點擊時「選單是開啟的」，那就把它關掉
+          if (nextEl.style.display == 'block') {
+            nextEl.style.display = 'none';
+          } else {
+            nextEl.style.display = 'block';
+          }
+        }
+      });
+    });
+  }
 });
